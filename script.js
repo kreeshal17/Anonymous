df706915b01e3d7308fe095f0d33d80077334c65
MR BLINDER <kreeshalkarna@gmail.com>
2025-02-15 03:59:19 +0530

    kree

--- script.js
+++ script.js
@@ -193,3 +193,11 @@ document.querySelectorAll('img').forEach(img => {
         this.src = 'https://via.placeholder.com/300x200?text=Image+Not+Found';
     };
 });
+document.getElementById("menu-toggle").addEventListener("click", function () {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+});
